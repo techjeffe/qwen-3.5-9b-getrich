f6fc62ffba88159e4c582446a14a6b6c41d23f94
Jeff Eberhard <28636257+techjeffe@users.noreply.github.com>
2026-05-13 19:31:27 -0500

    Removed Playwright for all items except Telegram snapshot sending.

--- backend/services/data_ingestion/__init__.py
+++ backend/services/data_ingestion/__init__.py
@@ -1,13 +1,11 @@
 """Data ingestion package initialization"""
 
-from .scraper import TruthSocialScraper
 from .parser import RSSFeedParser
 from .worker import build_analysis_posts, check_fast_lane, run_ingestion_cycle
 from .market_validation import MarketValidationClient
 from .yfinance_client import PriceClient
 
 __all__ = [
-    "TruthSocialScraper",
     "RSSFeedParser",
     "build_analysis_posts",
     "check_fast_lane",
